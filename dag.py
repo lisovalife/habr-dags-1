450d98dcee3fe5583d68455ae1787f3ee4bc150a
tlisova <lisova.tatiana.98@yandex.ru>
2024-12-16 11:37:23 +0300

    email operator test

--- dag.py
+++ dag.py
@@ -2,6 +2,8 @@ import airflow
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.email import EmailOperator
+
 
 default_args = {
     'owner': 'tlisova',
@@ -24,4 +26,11 @@ with DAG(
         python_callable = hello
     )
 
-task_1
+    Email = EmailOperator(
+        task_id='send_email',
+        to='tlisova@neoflex.ru',
+        subject='Airflow Alert',
+        html_content='<p>Your Airflow job has finished.</p>'
+    )
+
+task_1 -> Email
