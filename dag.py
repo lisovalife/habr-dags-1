262c2cb56360b7aa826899269a051ed4b72548be
tlisova <lisova.tatiana.98@yandex.ru>
2025-02-25 15:58:37 +0300

    test sqlSensor

--- dag.py
+++ dag.py
@@ -2,12 +2,17 @@ import airflow
 from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python_operator import PythonOperator
+from airflow.providers.telegram.operators.telegram import TelegramOperator
 from airflow.operators.email import EmailOperator
+from airflow.sensors.sql import SqlSensor
 
 
 default_args = {
     'owner': 'tlisova',
     'depends_on_past': False,
+    'email': ['tlisova@neoflex.ru'],
+    'email_on_failure': True,
+    'email_on_retry': True,
     'retries': 0,
     'max_active_runs': 1
 }
@@ -19,6 +24,7 @@ with DAG(
     dag_id = 'hello',
     default_args =default_args,
     catchup = False,
+    schedule_interval=None,
     tags = ['habr']
 ) as dag:
     task_1 = PythonOperator(
@@ -26,11 +32,25 @@ with DAG(
         python_callable = hello
     )
 
-    Email = EmailOperator(
-        task_id='send_email',
-        to='tlisova@neoflex.ru',
-        subject='Airflow Alert',
-        html_content='<p>Your Airflow job has finished.</p>'
+    # Email = EmailOperator(
+    #     task_id='send_email',
+    #     to='tlisova@neoflex.ru',
+    #     subject='Airflow Alert',
+    #     html_content='<p>Your Airflow job has finished.</p>'
+    # )
+
+
+    record_exists_sensor = SqlSensor(
+        task_id='check_record_exists',
+        conn_id='my_db_conn',
+        sql="""SELECT count(*)  FROM employees t WHERE date_of_recording > now()- INTERVAL '60 Minutes'""",
     )
 
-task_1 >> Email
+    telegram = TelegramOperator(
+        task_id="send_message_telegram",
+        telegram_conn_id="telegram_conn",
+        chat_id="1169795947",
+        text="Hello from Airflow!",
+    )
+
+task_1 >> record_exists_sensor >> telegram
